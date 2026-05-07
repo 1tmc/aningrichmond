551c2ec5a85ea65c4d704142d98b8e72d1e84f7b
1tmc <158778648+1tmc@users.noreply.github.com>
2026-05-07 19:19:18 +0200

    Updated portfolio design and content2

--- js/effect-1/index.js
+++ js/effect-1/index.js
@@ -1,21 +1,32 @@
 import { TextAnimator } from './text-animator.js';
 
+const isTouchDevice = () => window.matchMedia('(hover: none) and (pointer: coarse)').matches;
+
 const init = () => {
   document.querySelectorAll('.list__item').forEach(item => {
     const cols = Array.from(item.querySelectorAll('.hover-effect'));
     const animators = cols.map(col => new TextAnimator(col));
 
-    item.addEventListener('mouseenter', () => {
+    const triggerAnimation = () => {
       animators.forEach(animator => animator.animate());
-    });
+    };
+
+    item.addEventListener('mouseenter', triggerAnimation);
+
+    // Touch: trigger on tap
+    item.addEventListener('touchstart', (e) => {
+      triggerAnimation();
+    }, { passive: true });
   });
 
   // Same for all links
   document.querySelectorAll('a.hover-effect').forEach(item => {
     const animator = new TextAnimator(item);
-    item.addEventListener('mouseenter', () => {
-      animator.animate();
-    });
+
+    const triggerAnimation = () => animator.animate();
+
+    item.addEventListener('mouseenter', triggerAnimation);
+    item.addEventListener('touchstart', triggerAnimation, { passive: true });
   });
 };
 
